fb4cd07eca9fa269f90510001d11e0150ba59a22
jitender1322 <jitendersarswat6@gmail.com>
2025-07-31 18:02:20 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // 🔥 ADD THIS LINE
   plugins: [react()],
   server: {
     port: 3000,
